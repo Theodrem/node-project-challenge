d007a12ff17de6d4dc7567cd4ca79a7349dba463
Jessy <c.j91350@gmail.com>
2022-06-02 20:02:46 +0000

    Set eslint + prettier

--- src/api.ts
+++ src/api.ts
@@ -1,26 +1,23 @@
-import Express from "express";
-import swaggerUI from "swagger-ui-express";
+import Express from 'express';
+import swaggerUI from 'swagger-ui-express';
 import { RegisterRoutes } from './routes/routes';
 
 const PORT = process.env.PORT || 5050;
-
 const app = Express();
 
 RegisterRoutes(app);
 
-app.use(Express.static("public"));
+app.use(Express.static('public'));
 app.use(
-  "/docs",
+  '/docs',
   swaggerUI.serve,
   swaggerUI.setup(undefined, {
     swaggerOptions: {
-      url: "/swagger.json",
+      url: '/swagger.json',
     },
-  })
+  }),
 );
 
-app.listen(PORT,
-  () => {
-    console.info("API Listening on port " + PORT);
-  }
-);
+app.listen(PORT, () => {
+  console.info(`API Listening on port ${PORT}`);
+});
