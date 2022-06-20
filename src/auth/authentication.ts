93273a2ded1b04d72f630da9fbd70cfb2e04aa31
Clément <jacquelet.clement@outlook.fr>
2022-06-20 12:23:45 +0200

    feat(JWT) auth middleware

--- src/auth/authentication.ts
+++ src/auth/authentication.ts
@@ -1,18 +1,28 @@
-import { Request } from 'express';
-import { ApiError } from '../classes/ApiError';
-import { ErrorCode } from '../Type/api/ErrorCode';
+import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
+const config = require('../config/authConfig');
 
-export async function expressAuthentication(
-  request: Request,
-  securityName: string,
-  scopes?: string[]
-): Promise<boolean> {
+export function expressAuthentication(request: express.Request, securityName: string, scopes?: string[]) {
   if (securityName === 'jwt') {
-    if (!request.headers.authorization) {
-      throw new ApiError(ErrorCode.Unauthorized, 'auth/missing-header', 'Missing authorization header');
-    }
-    // TODO: Ajoutez votre propre logique de validation JWT
-  }
+    const token = request.body.token || request.query.token || request.headers['auth-token'];
 
-  return true;
+    return new Promise((resolve, reject) => {
+      if (!token) {
+        reject(new Error('No token provided'));
+      }
+      jwt.verify(token, config.secret, function (err: any, decoded: any) {
+        if (err) {
+          reject(err);
+        } else {
+          // Check if JWT contains all required scopes
+          for (let scope of scopes as string[]) {
+            if (!decoded.scopes.includes(scope)) {
+              reject(new Error('JWT does not contain required scope.'));
+            }
+          }
+          resolve(decoded);
+        }
+      });
+    });
+  }
 }
